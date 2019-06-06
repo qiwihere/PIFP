56eb45cf0eae20af086cad661d464efa078ffe04
qiwihere <qiwihere@gmail.com>
2019-06-07 00:21:32 +0300

    make an fft ops on patterned images

--- app.py
+++ app.py
@@ -1,36 +1,11 @@
 import random
 import lib
 import math
-import numpy
+import numpy as np
 from PIL import ImageDraw, Image, ImageOps
 
-#lib.generate_pattern_set("city.jpg",5,8,512)
 
-#lib.blured_image("city.jpg")
-
-temp_image = Image.open("city.jpg")
-draw = ImageDraw.Draw(temp_image)
-lib.draw_pattern(draw,45,5,512)
-temp_image.save("lined.jpg")
-
-
-f_image = Image.open("lined.jpg")
-pixels = numpy.array(f_image)
-
-fr = numpy.fft.fftn(pixels)
-
-size = fr.shape
-
-fr_real = numpy.empty(size)
-fr_imag = numpy.empty(size)
-
-for i in range(size[0]-1):
-    for j in range(size[1]-1):
-        for k in range(size[2]-1):
-            fr_real[i][j][k] = fr[i][j][k].real
-            fr_imag[i][j][k] = fr[i][j][k].imag
-
-
-un_fr = numpy.fft.ifftn(fr_real)
-
-print(fr_real[100][100])
+lib.generate_pattern_set("city.jpg", width=10, count=3, w=512, fftmult=300)
+'''
+Влияние малого количества пикселей на спектр изображения - Волков
+'''
--- lib.py
+++ lib.py
@@ -1,6 +1,8 @@
 from PIL import ImageDraw, Image, ImageOps
 import random
 import math
+import numpy as np
+import os, shutil
 
 
 def draw_line(ang, offset, width, draw, w=256):
@@ -29,18 +31,23 @@ def draw_pattern(draw, ang, width=5, w=256):
         draw_line(ang, i*width/w/1.5, width, draw, w)
 
 
-def generate_pattern_set(path, width, count= 6, w=256):
+def generate_pattern_set(path, width, count= 6, w=256, fftmult=1):
+    clear_dir('patterns')
+    clear_dir('ff')
+
     step = 360/count
 
     for i in range(round(-count/2), round(count/2)):
         ang = i*step
         temp_image = Image.open(path)
+        temp_image = blured_image(temp_image)
         draw = ImageDraw.Draw(temp_image)
         draw_pattern(draw, ang, width, w)
         temp_image.save("patterns/img_%s.jpg" % ang)
+        generate_fft("patterns/img_%s.jpg" % ang, fftmult)
 
-def blured_image(path):
-    image = Image.open(path)
+
+def blured_image(image):
     width, height = image.size
 
     n = 5
@@ -63,5 +70,38 @@ def blured_image(path):
                     g += pixel[i + k - (count + 1), j + x - (count + 1)][1] * matrix[k][x]
                     b += pixel[i + k - (count + 1), j + x - (count + 1)][2] * matrix[k][x]
             draw.point((i - (count), j - (count)), (int(r / n ** 2), int(g / n ** 2), int(b / n ** 2)))
+    return new_image
 
-    new_image.save("img_blured.jpg")
+
+def generate_fft(name, mult):
+    input_image = Image.open(name)
+    channels = input_image.split()  # splits an image into R, G, B channels
+    result_array = np.zeros_like(input_image)  # make sure data types,
+    # sizes and numbers of channels of input and output numpy arrays are the save
+
+    if len(channels) > 1:  # grayscale images have only one channel
+        for i, channel in enumerate(channels):
+            result_array[..., i] = fft(channel) * mult
+    else:
+        result_array[...] = fft(channels[0]) * mult
+
+    result_image = Image.fromarray(result_array)
+    result_image.save('ff/ff_%s' % name.split('/')[1])
+
+
+def fft(channel):
+    fft = np.fft.fft2(channel)
+    fft *= 255.0 / fft.max()  # proper scaling into 0..255 range
+    return np.absolute(fft)
+
+
+
+def clear_dir(folder):
+    for the_file in os.listdir(folder):
+        file_path = os.path.join(folder, the_file)
+        try:
+            if os.path.isfile(file_path):
+                os.unlink(file_path)
+            # elif os.path.isdir(file_path): shutil.rmtree(file_path)
+        except Exception as e:
+            print(e)
